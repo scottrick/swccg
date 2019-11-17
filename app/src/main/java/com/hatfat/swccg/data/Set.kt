d5db50878a78a396c40f781da3d052b756397c6f
Scott Atkins <scottrick49@gmail.com>
2019-11-17 15:58:59 -0600

    Added set json files.

---
+++ app/src/main/java/com/hatfat/swccg/data/Set.kt
@@ -0,0 +1,10 @@
+package com.hatfat.swccg.data
+
+data class Set(
+    val code: String,
+    val cycle_code: String,
+    val date_release: String,
+    val name: String,
+    val position: Int,
+    val size: Int
+)
